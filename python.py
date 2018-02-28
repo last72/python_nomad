a29f14b16413c5fd22672ea152a5103e967b3c99
Woongyeol Choi <cwy5847@gmail.com>
2018-02-28 11:25:44 +0800

    20180228 Testing
    Test

--- python.py
+++ python.py
@@ -3,4 +3,6 @@ choice = input('Enjoying the course? (y or n)')
 while choice != 'y' and choice != 'n':  # Fill in the condition (before the colon)
   choice = input("Sorry, I didn't catch that. Enter again: ")
 
-#change
+
+def functio()
+  return
