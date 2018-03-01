58e30156a620844841c09959f03056d6db3e1c0a
Woongyeol Choi <cwy5847@gmail.com>
2018-03-01 21:39:15 +0800

    codecademy

--- python.py
+++ python.py
@@ -1,8 +1,19 @@
-choice = input('Enjoying the course? (y or n)')
+# choice = input('Enjoying the course? (y or n)')
 
-while choice != 'y' and choice != 'n':  # Fill in the condition (before the colon)
-  choice = input("Sorry, I didn't catch that. Enter again: ")
+# while choice != 'y' and choice != 'n':  # Fill in the condition (before the colon)
+#   choice = input("Sorry, I didn't catch that. Enter again: ")
 
+text = input("Put string: ")
 
-def functio()
-  return
+def reverse(text):
+  lst = list(text)
+  tmp = []
+  for i in lst:
+    tmp = lst[len(lst)-1-i]
+    lst.append(tmp)
+    del(lst[len(lst)-1-i])
+  print (lst)
+  print (tmp)
+  return "".join(lst)
+
+reverse(text)
